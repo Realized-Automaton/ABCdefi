da055cce79bb5db69d64b4e39ab942022f79d3a2
Realized-Automaton <silverlinings905@yahoo.com>
2025-05-06 16:33:57 -0400

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+
 import type { Config } from "tailwindcss";
 
 export default {
@@ -10,7 +11,7 @@ export default {
   theme: {
   	extend: {
        fontFamily: {
-        sans: ['var(--font-dancing-script)', 'sans-serif'], // Add Dancing Script as primary sans font
+        sans: ['var(--font-roboto)', 'sans-serif'], // Change to Roboto font variable
       },
   		colors: {
   			background: 'hsl(var(--background))',
